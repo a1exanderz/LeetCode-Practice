41643bbbad0facc6a2eb58c3cb5fd04f2bb1a129
Alexander Zhang <awz2106@columbia.edu>
2022-08-22 12:29:54 -0700

    Time: 87 ms (50.26%), Space: 17.7 MB (31.08%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -7,20 +7,38 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        # Solution: O(n) time and O(n) memory
-        # Create a hashmap with all new values in the linked list. 
+        # Basic Solution: O(n^2) time and O(n) memory
+        # Create a list with all new values in the linked list. 
         # Traverse through the entire linked list.
-        # If a node already exists in the dictionary, return true.
+        # If a node already exists in the list, return true.
         # If next node is None and true not returned, then return false.
         
-        hashMap = []
-        curr = head
+#         hashMap = []
+#         curr = head
         
-        while curr is not None:
-            if curr in hashMap:
-                return True
-            else:
-                hashMap.append(curr)
-            curr = curr.next
-        return False
+#         while curr is not None:
+#             print(hashMap)
+#             if curr in hashMap:
+#                 return True
+#             else:
+#                 hashMap.append(curr)
+#             curr = curr.next
+#         return False
+        
+        # Using the turtle and rabbit solution: O(n) time and O(1) memory
+        # Create a turtle pointer and a rabbit pointer
+        # The rabbit pointer will move 2x every turn and turtle 1x
+        # If the two meet, then there is a loop, if fast reaches null then there is not
+        
+        turtle = head
+        rabbit = head
+        
+        while rabbit is not None and rabbit.next is not None:
+            print(turtle.val, rabbit.val)
+            turtle = turtle.next
+            rabbit = rabbit.next.next
             
+            if turtle == rabbit:
+                return True
+        
+        return False
