81f2366a024ca04fdb32fd5857c5bc82840c0e8a
Alexander Zhang <awz2106@columbia.edu>
2022-08-23 12:09:21 -0700

    Time: 155 ms (13.31%), Space: 14.1 MB (94.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -8,41 +8,42 @@ class Solution:
         # For each letter in ransomNote, subtract from the count in hashmap
             # If count ever goes below zero, return false
         
-#         hashMap = {}
-#         for c in magazine:
-#             if c in hashMap.keys():
-#                 hashMap[c] += 1
-#             else:
-#                 hashMap[c] = 1
+        hashMap = {}
+        for c in magazine:
+            if c in hashMap.keys():
+                hashMap[c] += 1
+            else:
+                hashMap[c] = 1
                 
-#         for c in ransomNote:
-#             if c not in hashMap.keys():
-#                 return False
-#             else:
-#                 hashMap[c] -= 1
-#                 if hashMap[c] < 0:
-#                     return False
+        for c in ransomNote:
+            if c not in hashMap.keys():
+                return False
+            else:
+                hashMap[c] -= 1
+                if hashMap[c] < 0:
+                    return False
         
-#         return True
+        return True
     
         # Solution 2: O(n) time, O(m) memory
         # Iterate through the ransomNote and magazine in one loop
         # Construct a hashmap of the magazine, if ransomNote char is in it, subtract from count
         # Same conditions above, but in one loop
         # DOES NOT WORK: ransom letter could be present in later part of magazine string -- or you could use sorted
+        # Video solution: just use my answer above
         
-        if len(ransomNote) > len(magazine):
-            return False
+#         if len(ransomNote) > len(magazine):
+#             return False
         
-        ran = sorted(ransomNote)
-        mag = sorted(magazine)
+#         ran = sorted(ransomNote)
+#         mag = sorted(magazine)
         
-        for c in mag:
-            if ran and c == ran[0]:
-                ran.pop(0)
+#         for c in mag:
+#             if ran and c == ran[0]:
+#                 ran.pop(0)
                 
-        if ran:
-            return False
-        else:
-            return True
+#         if ran:
+#             return False
+#         else:
+#             return True
     
