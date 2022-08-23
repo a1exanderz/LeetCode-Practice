a6c8bb047d7d4ea3920c6cf56fea4215103ac5f1
Alexander Zhang <awz2106@columbia.edu>
2022-08-23 13:08:40 -0700

    Time: 50 ms (39.97%), Space: 13.8 MB (57.48%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Solution:
+        # Solution O(n) time, O(n) memory:
         # Break into subproblems
         # Base: 1
         # To reach Step 1: 1 step
@@ -10,10 +10,20 @@ class Solution:
         # To reach Step 5: 1+1+1+1+1 steps or 1+2+1+1 steps or 1+1+2+1 steps or 2+1+1+1 steps or 1+1+1+2 steps or 2+2+1 steps or 2+1+2 steps or 1+2+2 steps
         # i.e. Fib sequence?
         
-        memo, i = [1, 1], 2
-        while i <= n:
-            print(i, memo)
-            memo.append(memo[i-2] + memo[i-1])
-            i += 1
+#         memo, i = [1, 1], 2
+#         while i <= n:
+#             memo.append(memo[i-2] + memo[i-1])
+#             i += 1
         
-        return memo[-1]
+#         return memo[-1]
+    
+    
+        # Solution with O(1) memory:
+        
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
