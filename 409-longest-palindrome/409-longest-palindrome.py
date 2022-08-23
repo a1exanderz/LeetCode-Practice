3d9a2c2de61c6c40340b508e958d0a9c20578d9b
Alexander Zhang <awz2106@columbia.edu>
2022-08-23 13:25:33 -0700

    Time: 55 ms (41.92%), Space: 13.8 MB (66.45%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        # Solution: O(2n) time, O(n) memory
+        # If length 1, return 1
+        # Only one odd count can exist, and rest have to be even
+        # For all even counts in string, add their entire value
+        # If there are multiple odd counts, the highest odd count add their entire value
+            # All other odd counts add their entire value - 1
+        
+        if len(s) == 1:
+            return 1
+        
+        hashTable = {}
+        for char in s:
+            if char in hashTable.keys():
+                hashTable[char] += 1
+            else:
+                hashTable[char] = 1
+        
+        totalLength = 0
+        oddCount = 0
+        for key in hashTable:
+            if hashTable[key] % 2 == 0:
+                totalLength += hashTable[key]
+            if hashTable[key] % 2 == 1:
+                oddCount += 1
+                totalLength += hashTable[key] - 1
+            
+        if oddCount > 0:
+            totalLength += 1
+            
+        return totalLength
+        
+        
+        
