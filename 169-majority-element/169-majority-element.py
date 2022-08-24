fa71f09c775d5586c02f04497650eda6cad4ed6a
Alexander Zhang <awz2106@columbia.edu>
2022-08-24 12:20:40 -0700

    Time: 380 ms (5.53%), Space: 15.4 MB (98.84%) - LeetHub

---
+++ 169-majority-element/169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Solution: O(n) time and O(n) space
+        # Find the majority threshold (n/2)
+        # Add each new value to hashtable and count occurrences
+        # At any point a value exceeds majority threshold you can return
+        
+        majorityNum = math.ceil(len(nums) / 2)
+        hashtable = {}
+        
+        if len(nums) == 1:
+            return nums[0]
+        
+        for num in nums:
+            if num in hashtable.keys(): 
+                hashtable[num] += 1
+                if hashtable[num] >= majorityNum:
+                    return num
+            else:
+                hashtable[num] = 1
+            
