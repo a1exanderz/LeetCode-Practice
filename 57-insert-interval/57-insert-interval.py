0912e86b85df206a8bb52b76dcabeb132e0a1431
Alexander Zhang <awz2106@columbia.edu>
2022-08-26 15:29:32 -0700

    Time: 77 ms (98.63%), Space: 17.3 MB (92.38%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,45 +1,61 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # If the intervals overlap, take minimum and maximum of the two intervals
+#         # If the intervals overlap, take minimum and maximum of the two intervals
         
-        output = []
+#         output = []
         
-        # If empty list
-        if not intervals:
-            output.append(newInterval)
-            return output
+#         # If empty list
+#         if not intervals:
+#             output.append(newInterval)
+#             return output
         
-        # If either before or after
-        if newInterval[1] < intervals[0][0]:
-            output.append(newInterval)
-            for interval in intervals:
-                output.append(interval)
-            return output
+#         # If either before or after
+#         if newInterval[1] < intervals[0][0]:
+#             output.append(newInterval)
+#             for interval in intervals:
+#                 output.append(interval)
+#             return output
                 
-        if newInterval[0] > intervals[-1][1]:
-            intervals.append(newInterval)
-            return intervals
+#         if newInterval[0] > intervals[-1][1]:
+#             intervals.append(newInterval)
+#             return intervals
         
-        # If in the middle, overlapping
-        merge = False
-        for interval in intervals:
-            if interval[0] < newInterval[0] and interval[1] < newInterval[0]:
-                output.append(interval)
-            elif interval[0] > newInterval[1] and interval[1] > newInterval[1]:
-                if not merge:
-                    output.append(newInterval)
-                    merge = True
-                output.append(interval)
-            else:
-                if not merge:
-                    output.append([interval[0], interval[1]])
-                    merge = True
+#         # If in the middle, overlapping
+#         merge = False
+#         for interval in intervals:
+#             if interval[0] < newInterval[0] and interval[1] < newInterval[0]:
+#                 output.append(interval)
+#             elif interval[0] > newInterval[1] and interval[1] > newInterval[1]:
+#                 if not merge:
+#                     output.append(newInterval)
+#                     merge = True
+#                 output.append(interval)
+#             else:
+#                 if not merge:
+#                     output.append([interval[0], interval[1]])
+#                     merge = True
                 
-                output[-1] = [min(output[-1][0], interval[0], newInterval[0]), max(output[-1][1], interval[1], newInterval[1])]
-                print("merge", interval[0], interval[1])
+#                 output[-1] = [min(output[-1][0], interval[0], newInterval[0]), max(output[-1][1], interval[1], newInterval[1])]
+#                 print("merge", interval[0], interval[1])
                 
-        return output
+#         return output
 
         #### PROPER METHOD
+        output = []
+        
+        for index, interval in enumerate(intervals):
+            if newInterval[1] < intervals[index][0]:
+                output.append(newInterval)
+                return output + intervals[index:]
+            elif newInterval[0] > intervals[index][1]:
+                output.append(intervals[index])
+            else:
+                newInterval = [min(newInterval[0], intervals[index][0]), max(newInterval[1], intervals[index][1])]
+        
+        output.append(newInterval)
+        
+        return output
+        
+        
         
 
