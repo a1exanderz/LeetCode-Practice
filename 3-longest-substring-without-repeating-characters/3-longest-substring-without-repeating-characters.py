f35dd2bc80557fb6e77722c32b2ff23a0d29fe36
Alexander Zhang <awz2106@columbia.edu>
2022-08-29 14:55:51 -0700

    Time: 1182 ms (7.42%), Space: 14 MB (49.86%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        # Simple solution: O(n^2) time, O(1) space
+        # For each letter in the string, check each subsequent letter until there is a repeat
+        # Return the max
+        
+        maxLength = 0
+        for i, c in enumerate(s):
+            length = 1
+            # print("index", i, "character", c)
+            i += 1
+            hashTable = {}
+            hashTable[c] = None
+            while i < len(s) and s[i] not in hashTable.keys():
+                hashTable[s[i]] = None
+                # print("hashtable", hashTable, "new index", i, "next character", s[i])
+                length += 1
+                # print("length", length)
+                i += 1
+            maxLength = max(length, maxLength)
+        
+        return maxLength
+    
+        # Need a faster solution
+                
