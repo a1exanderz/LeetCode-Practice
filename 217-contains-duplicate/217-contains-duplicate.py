da8d5b24b56f86c01592ad3c7381c42da361ffd8
Alexander Zhang <awz2106@columbia.edu>
2022-09-08 16:07:37 -0700

    Time: 455 ms (97.93%), Space: 25.9 MB (90.43%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,11 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        # Solution: 0(n) time and O(n) space
-        # For the entire list, add a value to the dictionary if it doesn't exist
-        # If the value already exists, return True
-        # If the entire loop runs, return False
-        # We use a dictionary for O(1) retrieval, vs. list which would take longer
+        # Use a hash table. If the value already exists in hash table, return True
+        # Otherwise return False if iterated through the entire array
         
         hashTable = {}
         
