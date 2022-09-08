89df8016b8c126f4daf62f276a7c2f2d44b965a2
Alexander Zhang <awz2106@columbia.edu>
2022-09-08 16:43:43 -0700

    Time: 289 ms (5.91%), Space: 27.3 MB (9.38%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,33 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        # Each anagram has its own equivalent hashTable
+        # Nested hashTables?
+        # The key is a hashTable, and values are strings that match that hashTable
+        # Then, append all values of each key in the main hashTable into output
+        
+        mainTable = {}
+        # Create a temporary hashTable for each string in the array
+        for string in strs:
+            tempTable = {}
+            for c in string:
+                if c in tempTable.keys():
+                    tempTable[c] += 1
+                else:
+                    tempTable[c] = 1
+
+            tempItems = frozenset(tempTable.items())
+
+            if tempItems not in mainTable.keys():
+                mainTable[tempItems] = [string]
+            else:
+                mainTable[tempItems].append(string)
+        
+        # Append each key's value to an output array
+        output = []
+        for key in mainTable:
+            output.append(mainTable[key])
+            
+        return output
+        
+        
