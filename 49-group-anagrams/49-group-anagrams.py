fc29ba5e5e2330b2b06378cbaf6618f1a504b44e
Alexander Zhang <awz2106@columbia.edu>
2022-09-08 16:55:44 -0700

    Time: 255 ms (8.92%), Space: 19.6 MB (29.22%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -6,28 +6,47 @@ class Solution:
         # The key is a hashTable, and values are strings that match that hashTable
         # Then, append all values of each key in the main hashTable into output
         
-        mainTable = {}
-        # Create a temporary hashTable for each string in the array
-        for string in strs:
-            tempTable = {}
-            for c in string:
-                if c in tempTable.keys():
-                    tempTable[c] += 1
-                else:
-                    tempTable[c] = 1
+#         mainTable = {}
+#         # Create a temporary hashTable for each string in the array
+#         for string in strs:
+#             tempTable = {}
+#             for c in string:
+#                 if c in tempTable.keys():
+#                     tempTable[c] += 1
+#                 else:
+#                     tempTable[c] = 1
 
-            # tempItems = frozenset(tempTable.items())
+#             # tempItems = frozenset(tempTable.items())
 
-            if frozenset(tempTable.items()) not in mainTable.keys():
-                mainTable[frozenset(tempTable.items())] = [string]
-            else:
-                mainTable[frozenset(tempTable.items())].append(string)
+#             if frozenset(tempTable.items()) not in mainTable.keys():
+#                 mainTable[frozenset(tempTable.items())] = [string]
+#             else:
+#                 mainTable[frozenset(tempTable.items())].append(string)
         
-        # Append each key's value to an output array
-        output = []
-        for key in mainTable:
-            output.append(mainTable[key])
+#         # Append each key's value to an output array
+#         output = []
+#         for key in mainTable:
+#             output.append(mainTable[key])
             
-        return output
+#         return output
+        
+    
+        ######## Solution without a nested hashTable #######
+        # Create a count array that has 26 blanks for each letter, for each string
+        # Add the count array to a main hashTable if new, otherwise append
+        
+        mainTable = {}
+        
+        for string in strs:
+            countArray = [0] * 26
+            for c in string:
+                countArray[ord(c) - ord('a')] += 1
+            
+            if tuple(countArray) not in mainTable.keys():
+                mainTable[tuple(countArray)] = [string]
+            else:
+                mainTable[tuple(countArray)].append(string)       
+                
+        return mainTable.values()
         
         
