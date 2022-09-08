569c95f14c9d57a1b4784ebec031d898558b6a9f
Alexander Zhang <awz2106@columbia.edu>
2022-09-08 16:56:52 -0700

    Time: 122 ms (81.66%), Space: 19.7 MB (27.50%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -6,42 +6,42 @@ class Solution:
         # The key is a hashTable, and values are strings that match that hashTable
         # Then, append all values of each key in the main hashTable into output
         
-        mainTable = {}
-        # Create a temporary hashTable for each string in the array
-        for string in strs:
-            tempTable = {}
-            for c in string:
-                if c in tempTable.keys():
-                    tempTable[c] += 1
-                else:
-                    tempTable[c] = 1
+#         mainTable = {}
+#         # Create a temporary hashTable for each string in the array
+#         for string in strs:
+#             tempTable = {}
+#             for c in string:
+#                 if c in tempTable.keys():
+#                     tempTable[c] += 1
+#                 else:
+#                     tempTable[c] = 1
 
-            # tempItems = frozenset(tempTable.items())
+#             # tempItems = frozenset(tempTable.items())
 
-            if frozenset(tempTable.items()) not in mainTable.keys():
-                mainTable[frozenset(tempTable.items())] = [string]
-            else:
-                mainTable[frozenset(tempTable.items())].append(string)
+#             if frozenset(tempTable.items()) not in mainTable.keys():
+#                 mainTable[frozenset(tempTable.items())] = [string]
+#             else:
+#                 mainTable[frozenset(tempTable.items())].append(string)
         
-        return mainTable.values()
+#         return mainTable.values()
         
     
         ######## Solution without a nested hashTable #######
         # Create a count array that has 26 blanks for each letter, for each string
         # Add the count array to a main hashTable if new, otherwise append
         
-#         mainTable = {}
+        mainTable = {}
         
-#         for string in strs:
-#             countArray = [0] * 26
-#             for c in string:
-#                 countArray[ord(c) - ord('a')] += 1
+        for string in strs:
+            countArray = [0] * 26
+            for c in string:
+                countArray[ord(c) - ord('a')] += 1
             
-#             if tuple(countArray) not in mainTable.keys():
-#                 mainTable[tuple(countArray)] = [string]
-#             else:
-#                 mainTable[tuple(countArray)].append(string)       
+            if tuple(countArray) not in mainTable.keys():
+                mainTable[tuple(countArray)] = [string]
+            else:
+                mainTable[tuple(countArray)].append(string)       
                 
-#         return mainTable.values()
+        return mainTable.values()
         
         
