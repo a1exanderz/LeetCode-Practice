7fcc0d1f275f9e30b7c6d107d656dcd6e803bc0c
Alexander Zhang <awz2106@columbia.edu>
2022-09-09 12:25:58 -0700

    Time: 181 ms (5.02%), Space: 14.4 MB (56.87%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,51 +1,36 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # Use a two pointer method
+        # Start from both ends of the string. If the characters are not equal, return false
         
-        start_pointer, end_pointer = 0, len(s) - 1
+        # If the indexes are the same, then the character must be the same
+        # If the indexes cross, end
         
-        while start_pointer < end_pointer:
-            
-            while not self.isAlphaNum(s[start_pointer]):
-                start_pointer += 1
-                
-                # Edge case of only special characters
-                if start_pointer == len(s) - 1:
-                    return True
-
-            while not self.isAlphaNum(s[end_pointer]):
-                end_pointer -= 1
-                
-            if s[start_pointer].lower() != s[end_pointer].lower():
+        fwd = 0
+        rev = len(s) - 1
+        
+        while fwd < rev:
+            while not self.alphanum(s[fwd]) and fwd < rev:
+                fwd += 1
+            while not self.alphanum(s[rev]) and fwd < rev:
+                rev -= 1
+            if s[fwd].lower() != s[rev].lower():
                 return False
             
-            start_pointer += 1
-            end_pointer -= 1
+            # print(fwd, s[fwd].lower(), rev, s[rev].lower())
+            
+            fwd += 1
+            rev -= 1
             
         return True
-
-    # If need to create your own isalnum function    
-    def isAlphaNum(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or 
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
-            
-        
-        
-        ### Straightforward solution using built in python string manipulation functions
-        
-#         # First, convert all uppercase letters into lowercase, remove all spaces and special characters. Runtime O(N), Memory O(N)
-#         converted_string = lower(''.join(ch for ch in s if ch.isalnum()))
-        
-#         # Compare to the palindrome version of the string, Runtime O(N), Memory O(N)
-#         return converted_string == converted_string[::-1]
     
-        
-        ### Attempting a new solution that uses less memory O(1) and does not use built in functions
-        ### Compare both ends of the string and return false if something doesn't match
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9")
+        )
             
-        
-        
+    
+    
+   
