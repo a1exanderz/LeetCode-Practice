6e4f92be4955b00ba35e77c5ddd10e4c110842d8
Alexander Zhang <awz2106@columbia.edu>
2022-09-09 15:44:29 -0700

    Time: 958 ms (71.39%), Space: 27.3 MB (79.90%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -10,11 +10,11 @@ class Solution:
         while l < r:
             area = (r - l) * min(height[l], height[r])
             maxArea = max(area, maxArea)
-            if height[l] < height[r]:
+            if height[l] <= height[r]:
                 l += 1
             else:
                 r -= 1
-            # print(area)
+            print(area)
         return maxArea
 
             
