3dbee4f5748bead85f69c4f5c62fa5e7c251014a
Alexander Zhang <awz2106@columbia.edu>
2022-09-09 17:59:43 -0700

    Time: 64 ms (93.50%), Space: 14 MB (49.78%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -2,31 +2,28 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         # Use a sliding window method
         # Have a left and right pointer, l r
+        # Iterate through the array with the right pointer
+        # If the right pointer exists in the set, 
+        
+        
         # If there are no repeats, continue incrementing r. When there is a repeat, l = r and r += 1 (start again).
             # Repeats determined through a set
             # Save the max length
+    
+    
+        maxLength = 0
+        l = 0
+        charSet = set()
         
-        if len(s) == 0:
-            return 0
-        
-        maxLength = 1
-        l, r = 0, 1
-        
-        while r < len(s):
-            unique = set({s[l]})
-            length = 1
-            while r < len(s) and s[r] not in unique:
-                length += 1
-                maxLength = max(maxLength, length)
-                unique.add(s[r])
-                r += 1
-            # print("l", l, "r", r, unique)
-            l += 1
-            r = l + 1
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            # print(charSet)
+            # print(r - l + 1)
+            maxLength = max(maxLength, r - l + 1)
             
         return maxLength
-    
-    # dvdf
-    # dv 
-            
         
+        # Optimized O(n^2) method
