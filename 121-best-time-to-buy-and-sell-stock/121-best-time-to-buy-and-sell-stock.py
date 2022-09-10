e7bca37b712ae176c266a2023a6c904ac1af6a05
Alexander Zhang <awz2106@columbia.edu>
2022-09-09 17:05:44 -0700

    Time: 1848 ms (32.53%), Space: 25 MB (85.68%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left, right = 0, 1
-        max_profit = 0
+        # 7 1 5 3 6 4
+        # Have two pointers (sliding window), l and r. 
+        # If prices[l] > prices[r], slide both of them to the right
+        # Otherwise keep incrementing r and save the profit and adjust maxProfit if needed
+        # Continue until r is at end of array
         
-        while right < len(prices):
-            if prices[right] > prices[left]:
-                profit = prices[right] - prices[left]
-                max_profit = max(max_profit, profit)
-            else:
-                left = right
-            right += 1
+        maxProfit = 0
+        l, r = 0, 1
+        while r < len(prices):
+            while prices[l] > prices[r] and r < len(prices) - 1:
+                l = r
+                r += 1
+            maxProfit = max(maxProfit, prices[r] - prices[l])
+            # print("l", prices[l], "r", prices[r])
+            r += 1
             
-        return max_profit
+        return maxProfit
+                
