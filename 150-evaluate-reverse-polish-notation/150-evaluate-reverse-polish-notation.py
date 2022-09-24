7d0703841ea3a82f703f222c66dd3691db020064
Alexander Zhang <awz2106@columbia.edu>
2022-09-24 15:24:50 -0700

    Time: 108 ms (58.59%), Space: 14.5 MB (29.61%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,36 +1,64 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # 2 1 + 3 *
-        # 3 * (1 + 2)
+#         # 2 1 + 3 *
+#         # 3 * (1 + 2)
         
-        # 4 13 5 / +
-        # (13 / 5) + 4
+#         # 4 13 5 / +
+#         # (13 / 5) + 4
         
-        # Every time there are 2 numbers followed by an operation, compute and reorganize
+#         # Every time there are 2 numbers followed by an operation, compute and reorganize
         
-        operations = "/*+-"
-        newTokens = []
+#         operations = "/*+-"
+#         newTokens = []
         
-        if len(tokens) == 1:
-            output = tokens[0]
-            return output
+#         if len(tokens) == 1:
+#             output = tokens[0]
+#             return output
     
-        for i in range(0, len(tokens)):
-            if tokens[i] in operations and tokens[i-1] not in operations and tokens[i-2] not in operations:
-                intOne, intTwo = int(tokens[i-2]), int(tokens[i-1])
-                newTokens.pop()
-                newTokens.pop()
-                if tokens[i] == "/":
-                    newTokens.append(str(int(float(intOne) / intTwo)))
-                elif tokens[i] == "*":
-                    newTokens.append(str(intOne * intTwo))
-                elif tokens[i] == "+":
-                    newTokens.append(str(intOne + intTwo))
-                elif tokens[i] == "-":
-                    newTokens.append(str(intOne - intTwo))
-            else:
-                newTokens.append(tokens[i])
-            # print(newTokens)
+#         for i in range(0, len(tokens)):
+#             if tokens[i] in operations and tokens[i-1] not in operations and tokens[i-2] not in operations:
+#                 intOne, intTwo = int(tokens[i-2]), int(tokens[i-1])
+#                 newTokens.pop()
+#                 newTokens.pop()
+#                 if tokens[i] == "/":
+#                     newTokens.append(str(int(float(intOne) / intTwo)))
+#                 elif tokens[i] == "*":
+#                     newTokens.append(str(intOne * intTwo))
+#                 elif tokens[i] == "+":
+#                     newTokens.append(str(intOne + intTwo))
+#                 elif tokens[i] == "-":
+#                     newTokens.append(str(intOne - intTwo))
+#             else:
+#                 newTokens.append(tokens[i])
+#             # print(newTokens)
         
-        return self.evalRPN(newTokens)
+#         return self.evalRPN(newTokens)
+
+        # Use a stack: every time you get an operation, you compute the two previous numbers
+        stack = []
+        operations = "/*+-"
+        for i in range(0, len(tokens)):
+            stack.append(tokens[i])
+            while stack[-1] in operations:
+                one, two = int(stack[-3]), int(stack[-2])
+                if stack[-1] == "/":
+                    stack.pop()
+                    stack.pop()
+                    stack[-1] = str(int(float(one) / two))
+                elif stack[-1] == "*":
+                    stack.pop()
+                    stack.pop()
+                    stack[-1] = str(one * two)
+                elif stack[-1] == "+":
+                    stack.pop()
+                    stack.pop()
+                    stack[-1] = str(one + two)
+                elif stack[-1] == "-":
+                    stack.pop()
+                    stack.pop()
+                    stack[-1] = str(one - two)
+        
+        return stack[0]
+            
+            
                     
