be2a38e44b2e6194fed17ada3f0d0ef1a2b2840f
Alexander Zhang <awz2106@columbia.edu>
2022-09-25 18:57:23 -0700

    Time: 521 ms (26.04%), Space: 22.9 MB (26.63%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,17 +1,34 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         
-        l, h = 0, len(nums) - 1
+#         ### Iterative Solution
+#         l, h = 0, len(nums) - 1
         
-        while l <= h:
-            m = (l + h) // 2
-            # print(l, m, h)
-            if nums[m] == target:
-                return m
-            elif nums[m] < target:
-                l = m + 1
+#         while l <= h:
+#             m = (l + h) // 2
+#             if nums[m] == target:
+#                 return m
+#             elif nums[m] < target:
+#                 l = m + 1
+#             else:
+#                 h = m - 1
+        
+#         return -1
+
+        ### Recursive Solution
+        low, high = 0, len(nums) - 1
+        
+        def binarySearch(low, high):
+            if high >= low:
+                mid = (low + high) // 2
+                if target > nums[mid]:
+                    return binarySearch(mid + 1, high)
+                elif target < nums[mid]:
+                    return binarySearch(low, mid - 1)
+                else:
+                    return mid
             else:
-                h = m - 1
+                return -1
         
-        return -1
+        return binarySearch(low, high)
         
