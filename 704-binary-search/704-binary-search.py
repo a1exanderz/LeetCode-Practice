63fab56c13a60e70e802f81b347e77e357d65293
Alexander Zhang <awz2106@columbia.edu>
2022-09-25 18:50:02 -0700

    Time: 660 ms (5.56%), Space: 15.5 MB (26.63%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,26 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Implement a traditional binary search algorithm on a sorted list (can do iteratively or recursively).
-        # Start from the middle index. If the target is higher than the middle index, search using the sublist above, and vice versa if target is below.
-        # Continue until target is found (m_index == target), or if not, return -1.
         
-        ### Recursive Method: Time = O(log(n))
+        l, h = 0, len(nums) - 1
         
-        low = 0
-        high = len(nums) - 1
-        
-        def binary(low, high):
-            if high >= low:
-                mid = (low + high) // 2
-        
-                if target == nums[mid]:
-                    return mid
-                elif target > nums[mid]:
-                    return binary(mid + 1, high)
-                else:
-                    return binary(low, mid - 1)
-
+        while l <= h:
+            m = (l + h) // 2
+            print(l, m, h)
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
             else:
-                return -1
-            
-        return binary(low, high)
+                h = m - 1
+        
+        return -1
+        
